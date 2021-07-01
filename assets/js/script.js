91c77860e71dfbb6e268302b6a92b5a01ff87ee5
David Szamek <davidszamek@gmail.com>
2021-07-01 16:35:04 +1000

    added open weather api key

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+// Global variables
+
+// Storing user input value 
+var city = $(".userInput").val();
+
+// Search button 
+var searchButton = $(".searchButton");
+
+// API Key 
+var apiKey = "67db64e8f2042018691a996ccaac0a12";

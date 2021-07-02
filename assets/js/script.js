9610d7d9b118fbe60efbb099b29c683749e30830
David Szamek <davidszamek@gmail.com>
2021-07-02 17:08:45 +1000

    fetch weather api and stored as an object

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,63 @@
-// Global variables
+$(document).ready(function () {
 
-// Storing user input value 
-var city = $(".userInput").val();
+})
+
+// Global variables
 
 // Search button 
 var searchButton = $(".searchButton");
+console.log(searchButton)
 
-// API Key 
+
+// Storing user input value 
+var cityInputEl = $("#cityInput");
+
+
+// API Key & URL 
 var apiKey = "67db64e8f2042018691a996ccaac0a12";
+
+var apiUrl
+
+// Fetch open weather api 
+
+function getWeather(event) {   
+    event.preventDefault();
+
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityInputEl.val() + "&units=metric&appid=" + apiKey;
+
+    console.log("working?");
+
+    fetch(apiUrl)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function(data) {
+        console.log(data);
+        // console.log(lat);
+        // console.log(lon);
+
+        // displayWeather(data, city);
+
+       
+    })
+    
+    // displayWeather();
+}
+
+var weatherEl = $("#current-weather-container");
+var userSearchEl = $("#userSearch")
+
+var displayWeather = function(weather, cityInputEl) {
+    weatherEl.textContent="";
+    userSearchEl.textContent=cityInputEl;
+
+    console.log(weather);
+
+    var currentDate = document.createElement("span")
+    currentDate.textContent=" (" + moment(weather.dt.value).format("D MMM, YYYY") + ") ";
+    userSearchEl.appendChild(currentDate);
+
+}
+
+
+searchButton.click(getWeather);
